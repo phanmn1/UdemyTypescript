f8d85d8a794b56eb64ab174d4586b2a73fcbf6b4
Minh Phan <mpflamingmarshmallow@gmail.com>
2020-09-22 03:13:53 -0700

    Advanced Generics

--- features/annotations/generics.ts
+++ features/annotations/generics.ts
@@ -1,10 +1,70 @@
-class HoldAnything<TypeOfData> {
-  data: TypeOfData
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
 }
 
-const holdNumber = new HoldAnything<number>();
-holdNumber.data = 123;
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
 
-const holdString = new HoldAnything<string>();
-holdString.data = 'kjlajdf;lj'
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
 
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arr = new ArrayOfAnything(['a', 'b', 'c'])
+
+// Example of generics with functions 
+
+function printStrings(arr: string[]): void {
+  for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+function printNumbers(arr: string[]): void {
+  for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+
+function printAnything<T>(arr: T[]): void {
+  for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+  }
+}
+
+// Generic Contraints
+class Car {
+  print() {
+    console.log('I am a car')
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a House')
+  }
+}
+
+interface Printable {
+  print(): void; 
+}
+
+// Has all the properties that type T has
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for(let i = 0; i < arr.length; ++i) {
+    arr[i].print()
+  }
+}
