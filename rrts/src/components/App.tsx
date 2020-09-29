d3c64668db4f32efbd11c4215c3f4a0f58e52c53
phanmn1 <mpflamingmarshmallow@gmail.com>
2020-09-28 17:50:07 -0700

    React/Redux Finish

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { ToDo, fetchTodos, deleteTodo } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: ToDo[];
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
+}
+
+interface AppState {
+  fetching: boolean;
+}
+
+export class _App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false };
+  }
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
+
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+    this.setState({ fetching: true });
+  };
+
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: ToDo) => {
+      return (
+        <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+          {todo.title}
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Hi There</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? 'LOADING' : null}
+        {this.renderList()}
+      </div>
+    );
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: ToDo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
---
+++ rrts/src/reducers/todos.ts
@@ -0,0 +1,12 @@
+import { ToDo, Action, ActionTypes } from '../actions';
+
+export const todosReducer = (state: ToDo[] = [], action: Action) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: ToDo) => todo.id !== action.payload);
+    default:
+      return state;
+  }
+};
